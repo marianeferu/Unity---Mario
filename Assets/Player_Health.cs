d0a8075fd76660f3094e27d630ebed5b70620a62
marianeferu <48251558+marianeferu@users.noreply.github.com>
2019-04-03 01:27:12 +0300

    Add files via upload

---
+++ Assets/Player_Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player_Health : MonoBehaviour {
+
+    public Text message;
+    public int health;
+    public bool hasDied;
+
+
+	// Use this for initialization
+	void Start () {
+        hasDied = false;
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (gameObject.transform.position.y < -5)
+        {
+            hasDied = true;
+
+        }
+
+        if (hasDied == true)
+        {
+            StartCoroutine("Die");
+        }
+    }
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(2);
+        message.enabled = true;
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("SampleScene");
+
+        yield return null;
+    }
+	}
---
+++ Assets/Player_Move.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour {
+
+    public int PlayerSpeed = 100;
+    private bool facingRight = true;
+    public int playerJumpPower = 1250;
+    private float moveX;
+
+	// Update is called once per frame
+	void Update () {
+
+        PlayerMove();
+
+	}
+
+    void PlayerMove()
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButton("Jump")) { Jump(); }
+
+
+        //animations
+
+
+        //player directions
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX >0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //physics
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+        //GetComponent<Rigidbody2D>.velocity.y
+    }
+
+    void Jump()
+    {
+        //int bool impact;
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale; 
+        localScale.x *= -1;
+        transform.localScale = localScale ;
+    }
+
+
+}
